3cafa1e515e4b921cf0691464cba66a3a8b97fc0
faithkatherine <faithcathy12@gmail.com>
2022-10-13 16:52:48 +0300

    new commit

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -27,15 +27,39 @@ export class RoomsComponent implements OnInit {
 
   }
 
-  roomList:RoomList = {
-    roomType: 'Deluxe Room',
-    amenities:'',
-    price: 0,
-    photos:'',
-    checkInTime: new Date('12- Oct-2022'),
-    checkOutTime: new Date('14-Oct-2022')
+  roomList:RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities:'Air Conditioner, Free WiFi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos:'https://unsplash.com/photos/p3UWyaujtQo',
+      checkInTime: new Date('12- Oct-2022'),
+      checkOutTime: new Date('14-Oct-2022')
+    },
 
-  }
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room',
+      amenities:'Air Conditioner, Free WiFi, TV, Bathroom, Kitchen',
+      price: 5000,
+      photos:'https://unsplash.com/photos/p3UWyaujtQo',
+      checkInTime: new Date('12- Oct-2022'),
+      checkOutTime: new Date('14-Oct-2022')
+
+    },
+
+    {
+      roomNumber: 3,
+      roomType: 'Private Suite',
+      amenities:'Air Conditioner, Free WiFi, TV, Bathroom, Kitchen',
+      price: 15000,
+      photos:'https://unsplash.com/photos/p3UWyaujtQo',
+      checkInTime: new Date('12- Oct-2022'),
+      checkOutTime: new Date('14-Oct-2022')
+    }
+
+  ]
 
   
 
